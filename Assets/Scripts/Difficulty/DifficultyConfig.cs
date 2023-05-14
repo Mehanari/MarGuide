798c6c787ca7d1a9549e79b2577016e1866520f2
Mehanari <102820106+Mehanari@users.noreply.github.com>
2023-05-14 15:34:57 +0300

    Win menu

---
+++ Assets/Scripts/Difficulty/DifficultyConfig.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class DifficultyConfig : ScriptableObject
+{
+   
+}
+
+
+[System.Serializable]
+public class DifficultyLevel
+{
+
+}
+
+public class GenerationParameters
+{
+
+}
